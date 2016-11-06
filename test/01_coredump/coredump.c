918f08226ce958b3c2bb09aa5feba8a9e51361ca
lyb <liyanboxin@yeah.net>
2016-11-07 00:28:28 +0800

    coredump test

---
+++ test/01_coredump/coredump.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int test1(void* p)
+{
+    int * p_int=(int *)p;
+    long val=(long)p;
+    
+    printf("[test1]val:%lu,p_int:%d",val,*p_int);
+    return 0;
+
+}
+
+int test2(int offset)
+{
+    long val=256+offset;
+    
+    test1((void *)val);
+    return 0;
+}
+
+
+
+int main(int argc, char **argv)
+{
+    printf("coredump test \n");
+    
+    test2(128);
+    return 0;
+}
+
+
